795d0a23742d21ff570823135273c14933ac28aa
mykola2312 <49044616+mykola2312@users.noreply.github.com>
2024-05-17 06:54:18 +0300

    Write ORM schema, apply indices on foreign keys since we're using text-based CUIDs

--- prisma/migrations/data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO "sm_app"."users"
-    ("id","created_at","updated_at","email","password","name")
-VALUES
-    ('clw8h3udm0000ci0xw2dscm07','2024-05-15 23:49:02.986','2024-05-15 23:49:02.986','test@sm-app.org','$argon2id$v=19$m=65536,t=3,p=4$iyrLqW6EOAZB3MEg/T2/7w$MT/874v31p588jYD1K+2kObaAceL5/39UEitDNi4TbE', NULL);

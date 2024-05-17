2947ba9ffa487802e443ffa00d96a3d2a8b0bce0
mykola2312 <49044616+mykola2312@users.noreply.github.com>
2024-05-17 07:06:47 +0300

    Write data migration

---
+++ data.sql
@@ -0,0 +1,5 @@
+INSERT INTO "sm_app"."users"
+    ("id","email","password","name","avatar","created_at","updated_at")
+VALUES
+    ('clw8h3udm0000ci0xw2dscm07','test1@sm-app.org','$argon2id$v=19$m=65536,t=3,p=4$iyrLqW6EOAZB3MEg/T2/7w$MT/874v31p588jYD1K+2kObaAceL5/39UEitDNi4TbE','Test User 1',NULL,'2024-05-15 23:49:02.986','2024-05-15 23:49:02.986'),
+    ('7ex9h3mdu0000ic0xw2dscm08','test2@sm-app.org','$argon2id$v=19$m=65536,t=3,p=4$iyrLqW6EOAZB3MEg/T2/7w$MT/874v31p588jYD1K+2kObaAceL5/39UEitDNi4TbE','Test User 2',NULL,'2024-05-15 23:49:02.986','2024-05-15 23:49:02.986');
